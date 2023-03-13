0127147ed91bf370a5b6057371078145499e52c5
Hongrui Zheng <billyzheng.bz@gmail.com>
2023-03-13 09:54:08 -0400

    updates

--- mpc/scripts/mpc_node.py
+++ mpc/scripts/mpc_node.py
@@ -66,6 +66,14 @@ class MPC(Node):
     def __init__(self):
         super().__init__('mpc_node')
         # TODO: create ROS subscribers and publishers
+        # TODO: get waypoints here
+        self.waypoints = None
+
+        self.config = mpc_config()
+        self.odelta_v = None
+        self.odelta = None
+        self.oa = None
+        self.init_flag = 0
 
         # initialize MPC problem
         self.mpc_prob_init()
@@ -80,6 +88,125 @@ class MPC(Node):
         # TODO: solve current iteration of optimization with reference trajectory
 
         # TODO: publish drive message, don't forget to limit the steering angle.
+
+    def mpc_prob_init(self):
+        """
+        Create MPC quadratic optimization problem using cvxpy, solver: OSQP
+        Will be solved every iteration for control.
+        More MPC problem information here: https://osqp.org/docs/examples/mpc.html
+        """
+        # Initialize and create vectors for the optimization problem
+        self.xk = cvxpy.Variable(
+            (self.config.NXK, self.config.TK + 1)
+        )  # Vehicle State Vector
+        self.uk = cvxpy.Variable(
+            (self.config.NU, self.config.TK)
+        )  # Control Input vector
+        objective = 0.0  # Objective value of the optimization problem, set to zero
+        constraints = []  # Create constraints array
+
+        # Initialize reference vectors
+        self.x0k = cvxpy.Parameter((self.config.NXK,))
+        self.x0k.value = np.zeros((self.config.NXK,))
+
+        # Initialize reference trajectory parameter
+        self.ref_traj_k = cvxpy.Parameter((self.config.NXK, self.config.TK + 1))
+        self.ref_traj_k.value = np.zeros((self.config.NXK, self.config.TK + 1))
+
+        # Initializes block diagonal form of R = [R, R, ..., R] (NU*T, NU*T)
+        R_block = block_diag(tuple([self.config.Rk] * self.config.TK))
+
+        # Initializes block diagonal form of Rd = [Rd, ..., Rd] (NU*(T-1), NU*(T-1))
+        Rd_block = block_diag(tuple([self.config.Rdk] * (self.config.TK - 1)))
+
+        # Initializes block diagonal form of Q = [Q, Q, ..., Qf] (NX*T, NX*T)
+        Q_block = [self.config.Qk] * (self.config.TK)
+        Q_block.append(self.config.Qfk)
+        Q_block = block_diag(tuple(Q_block))
+
+        # Formulate and create the finite-horizon optimal control problem (objective function)
+        # The FTOCP has the horizon of T timesteps
+        # TODO: fill in the objectives here, you should be using cvxpy.quad_form() somehwhere
+
+        # Objective 1: Influence of the control inputs: Inputs u multiplied by the penalty R
+        objective += cvxpy.quad_form(cvxpy.vec(self.uk), R_block)
+
+        # Objective 2: Deviation of the vehicle from the reference trajectory weighted by Q, including final Timestep T weighted by Qf
+        objective += cvxpy.quad_form(cvxpy.vec(self.xk - self.ref_traj_k), Q_block)
+
+        # Objective 3: Difference from one control input to the next control input weighted by Rd
+        objective += cvxpy.quad_form(cvxpy.vec(cvxpy.diff(self.uk, axis=1)), Rd_block)
+
+        # Constraints 1: Calculate the future vehicle behavior/states based on the vehicle dynamics model matrices
+        # Evaluate vehicle Dynamics for next T timesteps
+        A_block = []
+        B_block = []
+        C_block = []
+        # init path to zeros
+        path_predict = np.zeros((self.config.NXK, self.config.TK + 1))
+        for t in range(self.config.TK):
+            A, B, C = self.get_kinematic_model_matrix(
+                path_predict[2, t], path_predict[3, t], 0.0
+            )
+            A_block.append(A)
+            B_block.append(B)
+            C_block.extend(C)
+
+        A_block = block_diag(tuple(A_block))
+        B_block = block_diag(tuple(B_block))
+        C_block = np.array(C_block)
+
+        # [AA] Sparse matrix to CVX parameter for proper stuffing
+        # Reference: https://github.com/cvxpy/cvxpy/issues/1159#issuecomment-718925710
+        m, n = A_block.shape
+        self.Annz_k = cvxpy.Parameter(A_block.nnz)
+        data = np.ones(self.Annz_k.size)
+        rows = A_block.row * n + A_block.col
+        cols = np.arange(self.Annz_k.size)
+        Indexer = csc_matrix((data, (rows, cols)), shape=(m * n, self.Annz_k.size))
+
+        # Setting sparse matrix data
+        self.Annz_k.value = A_block.data
+
+        # Now we use this sparse version instead of the old A_ block matrix
+        self.Ak_ = cvxpy.reshape(Indexer @ self.Annz_k, (m, n), order="C")
+
+        # Same as A
+        m, n = B_block.shape
+        self.Bnnz_k = cvxpy.Parameter(B_block.nnz)
+        data = np.ones(self.Bnnz_k.size)
+        rows = B_block.row * n + B_block.col
+        cols = np.arange(self.Bnnz_k.size)
+        Indexer = csc_matrix((data, (rows, cols)), shape=(m * n, self.Bnnz_k.size))
+        self.Bk_ = cvxpy.reshape(Indexer @ self.Bnnz_k, (m, n), order="C")
+        self.Bnnz_k.value = B_block.data
+
+        # No need for sparse matrices for C as most values are parameters
+        self.Ck_ = cvxpy.Parameter(C_block.shape)
+        self.Ck_.value = C_block
+        # Add dynamics constraints to the optimization problem
+        constraints += [
+            cvxpy.vec(self.xk[:, 1:])
+            == self.Ak_ @ cvxpy.vec(self.xk[:, :-1])
+            + self.Bk_ @ cvxpy.vec(self.uk)
+            + (self.Ck_)
+        ]
+
+        constraints += [
+            cvxpy.abs(cvxpy.diff(self.uk[1, :]))
+            <= self.config.MAX_DSTEER * self.config.DTK
+        ]
+
+        # Create the constraints (upper and lower bounds of states and inputs) for the optimization problem
+        constraints += [self.xk[:, 0] == self.x0k]
+        constraints += [self.xk[2, :] <= self.config.MAX_SPEED]
+        constraints += [self.xk[2, :] >= self.config.MIN_SPEED]
+        constraints += [cvxpy.abs(self.uk[0, :]) <= self.config.MAX_ACCEL]
+        constraints += [cvxpy.abs(self.uk[1, :]) <= self.config.MAX_STEER]
+
+        # Create the optimization problem in CVXPY and setup the workspace
+        # Optimization goal: minimize the objective function
+        self.MPC_prob = cvxpy.Problem(cvxpy.Minimize(objective), constraints)
 
     def calc_ref_trajectory(self, state, waypoints):
         pass
@@ -114,3 +241,337 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+    def plan(self, states, waypoints=None):
+        """
+        Planner plan function overload for Pure Pursuit, returns acutation based on current state
+
+        Args:
+            pose_x (float): current vehicle x position
+            pose_y (float): current vehicle y position
+            pose_theta (float): current vehicle heading angle
+            lookahead_distance (float): lookahead distance to find next waypoint to track
+            waypoints (numpy.ndarray [N x 4], optional): list of dynamic waypoints to track, columns are [x, y, velocity, heading]
+
+        Returns:
+            speed (float): commanded vehicle longitudinal velocity
+            steering_angle (float):  commanded vehicle steering angle
+        """
+        if waypoints is not None:
+            if waypoints.shape[1] < 3 or len(waypoints.shape) != 2:
+                raise ValueError("Waypoints needs to be a (Nxm), m >= 3, numpy array!")
+            self.waypoints = waypoints
+        else:
+            if self.waypoints is None:
+                raise ValueError(
+                    "Please set waypoints to track during planner instantiation or when calling plan()"
+                )
+        vehicle_state = State(
+            x=states[0],
+            y=states[1],
+            delta=states[2],
+            v=states[3],
+            yaw=states[4],
+            yawrate=states[5],
+            beta=states[6],
+        )
+
+        (
+            speed,
+            steering_angle,
+            mpc_ref_path_x,
+            mpc_ref_path_y,
+            mpc_pred_x,
+            mpc_pred_y,
+            mpc_ox,
+            mpc_oy,
+        ) = self.MPC_Control_kinematic(vehicle_state, self.waypoints)
+
+        return steering_angle, speed
+
+    def calc_ref_trajectory_kinematic(self, state, cx, cy, cyaw, sp):
+        """
+        calc referent trajectory ref_traj in T steps: [x, y, v, yaw]
+        using the current velocity, calc the T points along the reference path
+        :param cx: Course X-Position
+        :param cy: Course y-Position
+        :param cyaw: Course Heading
+        :param sp: speed profile
+        :dl: distance step
+        :pind: Setpoint Index
+        :return: reference trajectory ref_traj, reference steering angle
+        """
+
+        # Create placeholder Arrays for the reference trajectory for T steps
+        ref_traj = np.zeros((self.config.NXK, self.config.TK + 1))
+        ncourse = len(cx)
+
+        # Find nearest index/setpoint from where the trajectories are calculated
+        _, _, _, ind = nearest_point(np.array([state.x, state.y]), np.array([cx, cy]).T)
+
+        # Load the initial parameters from the setpoint into the trajectory
+        ref_traj[0, 0] = cx[ind]
+        ref_traj[1, 0] = cy[ind]
+        ref_traj[2, 0] = sp[ind]
+        ref_traj[3, 0] = cyaw[ind]
+
+        # based on current velocity, distance traveled on the ref line between time steps
+        travel = abs(state.v) * self.config.DTK
+        dind = travel / self.config.dlk
+        ind_list = int(ind) + np.insert(
+            np.cumsum(np.repeat(dind, self.config.TK)), 0, 0
+        ).astype(int)
+        ind_list[ind_list >= ncourse] -= ncourse
+        ref_traj[0, :] = cx[ind_list]
+        ref_traj[1, :] = cy[ind_list]
+        ref_traj[2, :] = sp[ind_list]
+        cyaw[cyaw - state.yaw > 4.5] = np.abs(
+            cyaw[cyaw - state.yaw > 4.5] - (2 * np.pi)
+        )
+        cyaw[cyaw - state.yaw < -4.5] = np.abs(
+            cyaw[cyaw - state.yaw < -4.5] + (2 * np.pi)
+        )
+        ref_traj[3, :] = cyaw[ind_list]
+
+        return ref_traj
+
+    def predict_motion_kinematic(self, x0, oa, od, xref):
+        path_predict = xref * 0.0
+        for i, _ in enumerate(x0):
+            path_predict[i, 0] = x0[i]
+
+        state = State(x=x0[0], y=x0[1], yaw=x0[3], v=x0[2])
+        for (ai, di, i) in zip(oa, od, range(1, self.config.TK + 1)):
+            state = self.update_state_kinematic(state, ai, di)
+            path_predict[0, i] = state.x
+            path_predict[1, i] = state.y
+            path_predict[2, i] = state.v
+            path_predict[3, i] = state.yaw
+
+        return path_predict
+
+    def update_state_kinematic(self, state, a, delta):
+
+        # input check
+        if delta >= self.config.MAX_STEER:
+            delta = self.config.MAX_STEER
+        elif delta <= -self.config.MAX_STEER:
+            delta = -self.config.MAX_STEER
+
+        state.x = state.x + state.v * math.cos(state.yaw) * self.config.DTK
+        state.y = state.y + state.v * math.sin(state.yaw) * self.config.DTK
+        state.yaw = (
+            state.yaw + (state.v / self.config.WB) * math.tan(delta) * self.config.DTK
+        )
+        state.v = state.v + a * self.config.DTK
+
+        if state.v > self.config.MAX_SPEED:
+            state.v = self.config.MAX_SPEED
+        elif state.v < self.config.MIN_SPEED:
+            state.v = self.config.MIN_SPEED
+
+        return state
+
+    def get_kinematic_model_matrix(self, v, phi, delta):
+        """
+        Calc linear and discrete time dynamic model-> Explicit discrete time-invariant
+        Linear System: Xdot = Ax +Bu + C
+        State vector: x=[x, y, v, yaw]
+        :param v: speed
+        :param phi: heading angle of the vehicle
+        :param delta: steering angle: delta_bar
+        :return: A, B, C
+        """
+
+        # State (or system) matrix A, 4x4
+        A = np.zeros((self.config.NXK, self.config.NXK))
+        A[0, 0] = 1.0
+        A[1, 1] = 1.0
+        A[2, 2] = 1.0
+        A[3, 3] = 1.0
+        A[0, 2] = self.config.DTK * math.cos(phi)
+        A[0, 3] = -self.config.DTK * v * math.sin(phi)
+        A[1, 2] = self.config.DTK * math.sin(phi)
+        A[1, 3] = self.config.DTK * v * math.cos(phi)
+        A[3, 2] = self.config.DTK * math.tan(delta) / self.config.WB
+
+        # Input Matrix B; 4x2
+        B = np.zeros((self.config.NXK, self.config.NU))
+        B[2, 0] = self.config.DTK
+        B[3, 1] = self.config.DTK * v / (self.config.WB * math.cos(delta) ** 2)
+
+        C = np.zeros(self.config.NXK)
+        C[0] = self.config.DTK * v * math.sin(phi) * phi
+        C[1] = -self.config.DTK * v * math.cos(phi) * phi
+        C[3] = -self.config.DTK * v * delta / (self.config.WB * math.cos(delta) ** 2)
+
+        return A, B, C
+
+    def get_nparray_from_matrix(self, x):
+        return np.array(x).flatten()
+
+    
+
+    def mpc_prob_solve_kinematic(self, ref_traj, path_predict, x0):
+        self.x0k.value = x0
+
+        A_block = []
+        B_block = []
+        C_block = []
+        for t in range(self.config.TK):
+            A, B, C = self.get_kinematic_model_matrix(
+                path_predict[2, t], path_predict[3, t], 0.0
+            )
+            A_block.append(A)
+            B_block.append(B)
+            C_block.extend(C)
+
+        A_block = block_diag(tuple(A_block))
+        B_block = block_diag(tuple(B_block))
+        C_block = np.array(C_block)
+
+        self.Annz_k.value = A_block.data
+        self.Bnnz_k.value = B_block.data
+        self.Ck_.value = C_block
+
+        self.ref_traj_k.value = ref_traj
+
+        # Solve the optimization problem in CVXPY
+        # Solver selections: cvxpy.OSQP; cvxpy.GUROBI
+        self.MPC_prob.solve(solver=cvxpy.OSQP, verbose=False, warm_start=True)
+
+        if (
+            self.MPC_prob.status == cvxpy.OPTIMAL
+            or self.MPC_prob.status == cvxpy.OPTIMAL_INACCURATE
+        ):
+            ox = self.get_nparray_from_matrix(self.xk.value[0, :])
+            oy = self.get_nparray_from_matrix(self.xk.value[1, :])
+            ov = self.get_nparray_from_matrix(self.xk.value[2, :])
+            oyaw = self.get_nparray_from_matrix(self.xk.value[3, :])
+            oa = self.get_nparray_from_matrix(self.uk.value[0, :])
+            odelta = self.get_nparray_from_matrix(self.uk.value[1, :])
+
+        else:
+            print("Error: Cannot solve mpc..")
+            oa, odelta, ox, oy, oyaw, ov = None, None, None, None, None, None
+
+        return oa, odelta, ox, oy, oyaw, ov
+
+    def linear_mpc_control_kinematic(self, ref_path, x0, oa, od):
+        """
+        MPC contorl with updating operational point iteraitvely
+        :param ref_path: reference trajectory in T steps
+        :param x0: initial state vector
+        :param a_old: acceleration of T steps of last time
+        :param delta_old: delta of T steps of last time
+        :return: acceleration and delta strategy based on current information
+        """
+
+        if oa is None or od is None:
+            oa = [0.0] * self.config.TK
+            od = [0.0] * self.config.TK
+
+        # Call the Motion Prediction function: Predict the vehicle motion for x-steps
+        path_predict = self.predict_motion_kinematic(x0, oa, od, ref_path)
+        poa, pod = oa[:], od[:]
+
+        # Run the MPC optimization: Create and solve the optimization problem
+        mpc_a, mpc_delta, mpc_x, mpc_y, mpc_yaw, mpc_v = self.mpc_prob_solve_kinematic(
+            ref_path, path_predict, x0
+        )
+
+        return mpc_a, mpc_delta, mpc_x, mpc_y, mpc_yaw, mpc_v, path_predict
+
+    def MPC_Control_kinematic(self, vehicle_state, path):
+        # Extract information about the trajectory that needs to be followed
+        cx = path[0]  # Trajectory x-Position
+        cy = path[1]  # Trajectory y-Position
+        cyaw = path[2]  # Trajectory Heading angle
+        sp = path[3]  # Trajectory Velocity
+
+        # Calculate the next reference trajectory for the next T steps:: [x, y, v, yaw]
+        ref_path = self.calc_ref_trajectory_kinematic(vehicle_state, cx, cy, cyaw, sp)
+        # Create state vector based on current vehicle state: x-position, y-position,  velocity, heading
+        x0 = [vehicle_state.x, vehicle_state.y, vehicle_state.v, vehicle_state.yaw]
+
+        # Solve the Linear MPC Control problem
+        (
+            self.oa,
+            self.odelta_v,
+            ox,
+            oy,
+            oyaw,
+            ov,
+            state_predict,
+        ) = self.linear_mpc_control_kinematic(ref_path, x0, self.oa, self.odelta_v)
+
+        if self.odelta_v is not None:
+            di, ai = self.odelta_v[0], self.oa[0]
+
+        # Create the final steer and speed parameter that need to be sent out
+
+        # Steering Output: First entry of the MPC steering angle output vector in degree
+        steer_output = self.odelta_v[0]
+
+        speed_output = vehicle_state.v + self.oa[0] * self.config.DTK
+
+        if self.debug:
+            plt.cla()
+            plt.axis(
+                [
+                    vehicle_state.x - 6,
+                    vehicle_state.x + 4.5,
+                    vehicle_state.y - 2.5,
+                    vehicle_state.y + 2.5,
+                ]
+            )
+            plt.plot(
+                cx,
+                cy,
+                linestyle="solid",
+                linewidth=2,
+                color="#005293",
+                label="Raceline",
+            )
+            plt.plot(
+                vehicle_state.x,
+                vehicle_state.y,
+                marker="o",
+                markersize=10,
+                color="red",
+                label="CoG",
+            )
+            plt.scatter(
+                ref_path[0],
+                ref_path[1],
+                marker="x",
+                linewidth=4,
+                color="purple",
+                label="MPC Input: Ref. Trajectory for T steps",
+            )
+            plt.scatter(
+                ox,
+                oy,
+                marker="o",
+                linewidth=4,
+                color="green",
+                label="MPC Output: Trajectory for T steps",
+            )
+            plt.legend()
+            plt.pause(0.000001)
+            plt.axis("equal")
+
+        return (
+            speed_output,
+            steer_output,
+            ref_path[0],
+            ref_path[1],
+            state_predict[0],
+            state_predict[1],
+            ox,
+            oy,
+        )
